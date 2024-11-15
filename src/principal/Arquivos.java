9e740f28f3c117cd2a80bf0f8c90f336d27312fe
gabrielhr77 <gabrielhr@usp.br>
2024-11-15 19:42:59 -0300

    juntando front com back

---
+++ src/principal/Arquivos.java
@@ -0,0 +1,260 @@
+package principal;
+
+import principal.Main;
+import entidades.Ator;
+import entidades.Diretor;
+import entidades.Filme;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class Arquivos {
+	
+//	ArrayList<Filme> todosOsFilmes = new ArrayList<>();
+//	ArrayList<Ator> todosOsAtores = new ArrayList<>();
+//	ArrayList<Diretor> todosOsDiretores = new ArrayList<>();
+	
+	static boolean flagLeitorFilmes = false;
+//	boolean flagLeitorUsuarios = false;
+
+	static String caminhoArquivoFilmes = Main.retornaCaminhoArquivo("filme");
+//	String caminhoArquivoUsuarios = Main.retornaCaminhoArquivo("usuario");
+	
+	static BufferedReader leitorFilmes;// = new BufferedReader(new FileReader(caminhoArquivoFilmes));
+//	BufferedReader leitorUsuarios;// = new BufferedReader(new FileReader(caminhoArquivoUsuarios));
+
+	static String linha;
+	
+//	aqui vou aplicar uma "técnica" que um usuário do stack overflow recomendou, que é a LAZY LOADING,
+//	ou seja, só inicializar o leitor quando for usá-lo, não diretamente no corpo principal da classe
+	
+	public static BufferedReader getLeitorFilmesAtoresDiretores() {
+		try {
+			leitorFilmes = new BufferedReader(new FileReader(caminhoArquivoFilmes));	
+			flagLeitorFilmes = false;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return leitorFilmes;
+	}
+//	public BufferedReader getLeitorUsuarios() {
+//		try {
+//			leitorUsuarios = new BufferedReader(new FileReader(caminhoArquivoUsuarios));	
+//			flagLeitorUsuarios = false;
+//		} catch(IOException e) {
+//			e.printStackTrace();
+//		}
+//		return leitorUsuarios;
+//	}
+
+	
+	
+	//função para fechar os leitores após o uso
+	public static void fechaLeitores() throws IOException {
+		if(flagLeitorFilmes) {
+			leitorFilmes.close();
+		}
+//		else if(flagLeitorUsuarios) {
+//			leitorUsuarios.close();
+//		}
+	}
+	
+	//TENHO QUE TER UMA FUNÇÃO IGUAL A ESSA NA CLASSE DE MEIO-CAMPO DOS USUÁRIOS
+	//função que copia os dados do arquivo CSV e os coloca dentro do arraylist	
+	public static void copiaDadosDoArquivo(String caminho, BufferedReader leitor, ArrayList<Filme> arraylistFilmes, ArrayList<Ator> arraylistAtores, ArrayList<Diretor> arraylistDiretores) throws IOException{
+		boolean primeiraLinha = true;
+		boolean primeiroCicloFilmes = true;
+		boolean primeiroCicloAtores = true;
+		boolean primeiroCicloDiretores = true;
+		
+		boolean jaExiste = false;
+		boolean jaExisteAtor1 = false;
+		boolean jaExisteAtor2= false;
+		boolean jaExisteAtor3 = false;
+		while((linha = leitor.readLine())!=null) {
+			//ignoramos o cabecalho
+			if(primeiraLinha) {
+				primeiraLinha = false;
+				continue;
+			}
+			//agora pego o ; para separar as frases da linha para achar qual a linha do filme em questão
+			String[] partesDaLinha = linha.split(";");
+			//esse ".class" vai retornar um objeto da classe correnspondente em TEMPO DE EXECUÇÃO, ou seja, a classe genérica terá seu tipo explicitado durante o programa
+				
+			flagLeitorFilmes = true;
+			LocalDate dataAtor1 = LocalDate.parse(partesDaLinha[9]);
+			LocalDate dataAtor2 = LocalDate.parse(partesDaLinha[12]);
+			LocalDate dataAtor3 = LocalDate.parse(partesDaLinha[15]);
+			LocalDate dataDiretor = LocalDate.parse(partesDaLinha[6]);
+			
+			Ator ator1 = new Ator(Integer.parseInt(partesDaLinha[7]),partesDaLinha[8],dataAtor1);
+			Ator ator2 = new Ator(Integer.parseInt(partesDaLinha[10]),partesDaLinha[11],dataAtor2);
+			Ator ator3 = new Ator(Integer.parseInt(partesDaLinha[13]),partesDaLinha[14],dataAtor3);
+			
+			if(!primeiroCicloAtores) {
+				for(int i=0;i<arraylistAtores.size();i++) {
+					if(arraylistAtores.get(i).getIDAtor()==ator1.getIDAtor()) {
+						jaExisteAtor1=true;
+					}
+					if(arraylistAtores.get(i).getIDAtor()==ator2.getIDAtor()) {
+						jaExisteAtor2=true;
+					}
+					if(arraylistAtores.get(i).getIDAtor()==ator3.getIDAtor()) {
+						jaExisteAtor3=true;
+					}
+				}
+				if(!jaExisteAtor1) {
+					arraylistAtores.add(ator1);
+				}
+				if(!jaExisteAtor2) {
+					arraylistAtores.add(ator2);
+				}
+				if(!jaExisteAtor3) {
+					arraylistAtores.add(ator3);
+				}
+			}else {
+				arraylistAtores.add(ator1);
+				arraylistAtores.add(ator2);
+				arraylistAtores.add(ator3);
+			}
+			primeiroCicloAtores = false;
+			jaExisteAtor1=false;
+			jaExisteAtor2=false;
+			jaExisteAtor3=false;
+			
+			
+			Diretor diretor = new Diretor(Integer.parseInt(partesDaLinha[4]),partesDaLinha[5],dataDiretor);
+			if(!primeiroCicloDiretores)	{
+				for(int i=0;i<arraylistDiretores.size();i++) {
+					if(arraylistDiretores.get(i).getIDDiretor()==diretor.getIDDiretor()) {
+						jaExiste=true;
+					}
+				}
+				if(!jaExiste) {
+					arraylistDiretores.add(diretor);
+				}
+				jaExiste=false;	
+			}else {
+				arraylistDiretores.add(diretor);
+			}
+			
+			//verifica se o filme já existe, se não existir ele adiciona ao arraylist
+			Filme filme = new Filme(Integer.parseInt(partesDaLinha[0]),partesDaLinha[1],partesDaLinha[2],diretor,Integer.parseInt(partesDaLinha[3]),ator1,ator2,ator3);
+			
+			if(!primeiroCicloFilmes) {
+				for(int i=0;i<arraylistFilmes.size();i++) {
+					if(arraylistFilmes.get(i).getIDFilme()==filme.getIDFilme()) {
+						jaExiste=true;
+					}
+				}
+				if(!jaExiste) {
+					arraylistFilmes.add(filme);
+				}
+			
+				jaExiste=false;
+			}else {
+				arraylistFilmes.add(filme);
+			}
+			
+		}
+		fechaLeitores();
+
+	}
+	
+	
+	//função que salva os dados do arraylist no arquivo CSV ao finalizar o programa
+	public static <T> void salvaDadosNoArquivo(String caminho, File arquivo, ArrayList<Filme> lista) throws IOException {
+		boolean existe = arquivo.exists();
+		//abro o escritor com o Charset de símbolos do português
+		FileWriter escritor = new FileWriter(caminho, StandardCharsets.ISO_8859_1,existe);
+		if(!existe) {
+			escritor.write("FILME_ID;FILME_NOME;GENERO;ANO_LANCAMENTO;DIRETOR_ID;DIRETOR_NOME;DIR_DATA_NASC;ATOR1_ID;ATOR1_NOME;ATOR1_DATA_NASC;ATOR2_ID;ATOR2_NOME;ATOR2_DATA_NASC;ATOR3_ID;ATOR3_NOME;ATOR3_DATA_NASC\n");
+		}
+		//escrevo os dados no arquivo
+		for(int i=0;i<lista.size();i++) {
+			escritor.write(lista.get(i).getIDFilme()+";"+lista.get(i).getNomeFilme()+";"+lista.get(i).getGeneroFilme()+";"+lista.get(i).getAnoLancamento()+";"+lista.get(i).getDiretor().getIDDiretor()+";"+lista.get(i).getDiretor().getNomeDiretor()+";"+lista.get(i).getDiretor().getDataNascimentoDiretor()+";"+lista.get(i).getAtor1().getIDAtor()+";"+lista.get(i).getAtor1().getNomeAtor()+";"+lista.get(i).getAtor1().getDataNascimentoAtor()+";"+lista.get(i).getAtor2().getIDAtor()+";"+lista.get(i).getAtor2().getNomeAtor()+";"+lista.get(i).getAtor2().getDataNascimentoAtor()+";"+lista.get(i).getAtor3().getIDAtor()+";"+lista.get(i).getAtor3().getNomeAtor()+";"+lista.get(i).getAtor3().getDataNascimentoAtor()+"\n");
+			escritor.flush();
+		}
+		escritor.close();
+	
+	
+	}
+	
+	
+	
+//	//função para adicionar filme
+//	public static void adicionaFilme(Filme filme, ArrayList<Filme> arraylistFilmes) {
+//		arraylistFilmes.add(filme);
+//	}
+//	
+//	//função para remover filme
+//	public static void removeFilme(String nomeFilme, ArrayList<Filme> arraylistFilmes) {
+//		int i = buscaFilmePorNome(nomeFilme,arraylistFilmes);
+//		arraylistFilmes.remove(i);
+//	}
+//	
+//	//função para encontrar a posição do filme dentro do arraylist com todos os filmes
+//	private static int buscaFilmePorNome(String nome, ArrayList<Filme> arraylistFilmes) {
+//		for(int i=0;i<arraylistFilmes.size();i++) {
+//			if(arraylistFilmes.get(i).getNomeFilme().equals(nome)) {
+//				return i;
+//			}
+//        }
+//		return -1;
+//	}
+//
+//	
+//	//função para adicionar diretor
+//	public static void adicionaDiretor(Diretor diretor, ArrayList<Diretor> arraylistDiretores) {
+//		arraylistDiretores.add(diretor);
+//	}
+//	
+//	//função para remover diretor
+//	public static void removeDiretor(String nomeDiretor, ArrayList<Diretor> arraylistDiretores) {
+//		int i = buscaDiretorPorNome(nomeDiretor,arraylistDiretores);
+//		arraylistDiretores.remove(i);
+//	}
+//	
+//	//função para encontrar a posição do diretor dentro do arraylist com todos os diretores
+//	private static int buscaDiretorPorNome(String nome, ArrayList<Diretor> arraylistDiretores) {
+//		for(int i=0;i<arraylistDiretores.size();i++) {
+//			if(arraylistDiretores.get(i).getNomeDiretor().equals(nome)) {
+//				return i;
+//			}
+//        }
+//		return -1;
+//	}
+//	
+//	
+//	//função para adicionar ator
+//	public static void adicionaAtor(Ator ator, ArrayList<Ator> arraylistAtores) {
+//			arraylistAtores.add(ator);
+//		}
+//		
+//	//função para remover ator
+//	public static void removeAtor(String nomeAtor, ArrayList<Ator> arraylistAtores) {
+//		int i = buscaAtorPorNome(nomeAtor,arraylistAtores);
+//		arraylistAtores.remove(i);
+//	}
+//	
+//	//função para encontrar a posição do ator dentro do arraylist com todos os atores
+//	private static int buscaAtorPorNome(String nome, ArrayList<Ator> arraylistAtores) {
+//		for(int i=0;i<arraylistAtores.size();i++) {
+//			if(arraylistAtores.get(i).getNomeAtor().equals(nome)) {
+//				return i;
+//			}
+//        }
+//		return -1;
+//	}
+
+}
+	
+
+
---
+++ src/principal/Main.java
@@ -0,0 +1,99 @@
+package principal;
+import entidades.Filme;
+import entidades.FilmeCSV;
+import entidades.Pessoa;
+import entidades.Diretor;
+import entidades.Ator;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+	//variáveis globais que podem ser vistas por qualquer método na main
+	public static final String caminhoArquivoFilmes = "./dados/Filmes.csv";
+	public static final String caminhoArquivoUsuarios = "./dados/Usuarios.csv";
+	
+	public static void main(String[] args) throws IOException {
+		
+		//*************************************************************************************
+		//abrir os 2 arquivos 
+		//*************************************************************************************
+		//FILMES
+		File arquivoFilmes = new File(caminhoArquivoFilmes);
+		boolean existeArquivoFilmes = arquivoFilmes.exists();
+		FileWriter escritorFilmes = new FileWriter(caminhoArquivoFilmes, StandardCharsets.ISO_8859_1, existeArquivoFilmes);
+		if(!existeArquivoFilmes) {
+			escritorFilmes.write("FILME_ID;FILME_NOME;GENERO;ANO_LANCAMENTO;DIRETOR_ID;DIRETOR_NOME;DIR_DATA_NASC;ATOR1_ID;ATOR1_NOME;ATOR1_DATA_NASC;ATOR2_ID;ATOR2_NOME;ATOR2_DATA_NASC;ATOR3_ID;ATOR3_NOME;ATOR3_DATA_NASC\n");
+			escritorFilmes.close();
+		}
+		//*************************************************************************************
+		//USUARIOS
+		File arquivoUsuarios = new File(caminhoArquivoUsuarios);
+		boolean existeArquivoUsuarios = arquivoUsuarios.exists();
+		FileWriter escritorDiretores = new FileWriter(caminhoArquivoUsuarios, StandardCharsets.ISO_8859_1, existeArquivoUsuarios);
+		if(!existeArquivoUsuarios) {
+			escritorDiretores.write("FILME_ID;NOME;GENERO;DIRETOR;ANO_LANCAMENTO;ATOR1;ATOR2;ATOR3;novacoluna\n");
+			escritorDiretores.close();
+		}
+		//*************************************************************************************	
+		//*************************************************************************************
+		//criação dos arraylists
+		
+		ArrayList<Filme> todosOsFilmes = new ArrayList<>();
+		ArrayList<Ator> todosOsAtores = new ArrayList<>();
+		ArrayList<Diretor> todosOsDiretores = new ArrayList<>();
+		
+		//*************************************************************************************
+		//copiar os dados dos arquivos para os arraylists 
+		//*************************************************************************************
+		
+		Arquivos.copiaDadosDoArquivo(caminhoArquivoFilmes,Arquivos.getLeitorFilmesAtoresDiretores(),todosOsFilmes,todosOsAtores,todosOsDiretores);
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		LocalDate dateAtor1 = LocalDate.of(1971,6,5);
+		Ator ator1 = new Ator(60001,"Mark Wahlberg",dateAtor1);
+		
+		LocalDate dateAtor2 = LocalDate.of(1982,1,7);
+		Ator ator2 = new Ator(60002,"Lauren Cohan",dateAtor2);
+		
+		LocalDate dateAtor3 = LocalDate.of(1983,2,12);
+		Ator ator3 = new Ator(60003,"Iko Uwais",dateAtor3);
+		
+		LocalDate dateDiretor1 = LocalDate.of(1964,3,11);
+		Diretor Peter_Berg = new Diretor(50001,"Peter Berg", dateDiretor1);
+		
+		Filme filme1 = new Filme(20001,"22 milhas","acao",Peter_Berg ,2021, ator1, ator2, ator3);
+		Filme filme2 = new Filme(20002,"King Kong","acao",Peter_Berg ,2016, ator1, ator2, ator3);
+		Filme filme3 = new Filme(20003,"Norbit","terror",Peter_Berg ,2010, ator1, ator2, ator3);
+		
+		FilmeCSV filmeCSV = new FilmeCSV(); 
+//		Catalogo catalogo = new Catalogo();
+		ManipulacaoDados.adicionaFilme(filme1,todosOsFilmes);
+		ManipulacaoDados.adicionaFilme(filme2,todosOsFilmes);
+		ManipulacaoDados.adicionaFilme(filme3,todosOsFilmes);
+		filmeCSV.imprimirFilme(20003);
+		Arquivos.salvaDadosNoArquivo(caminhoArquivoFilmes, arquivoFilmes, todosOsFilmes);
+		
+		
+		
+	}
+
+	
+	//função que permite enviar o caminho dos arquivos à classe Catalogo
+	public static final String retornaCaminhoArquivo(String entidadeDoArquivo) {
+		switch(entidadeDoArquivo) {
+		case "filme":
+			return caminhoArquivoFilmes;
+		case "diretor":
+			return caminhoArquivoUsuarios;
+		default: 
+			return "-1";
+		}
+	}
+}
